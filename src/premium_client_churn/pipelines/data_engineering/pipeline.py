33b9057af009aee006591214c9a88ed7d9b0f256
ultraxav <ultraxav@msn.com>
2021-09-05 17:31:22 -0300

    first node add

--- src/premium_client_churn/pipelines/data_engineering/pipeline.py
+++ src/premium_client_churn/pipelines/data_engineering/pipeline.py
@@ -1,21 +1,16 @@
 # libs
 from kedro.pipeline import Pipeline, node
-from .nodes import split_data
+from .nodes import clean_data
 
 # pipelines
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                split_data,
-                ["example_iris_data", "params:example_test_data_ratio"],
-                dict(
-                    train_x="example_train_x",
-                    train_y="example_train_y",
-                    test_x="example_test_x",
-                    test_y="example_test_y",
-                ),
-                name="split",
+                clean_data,
+                inputs=['raw_data', 'params:raw_data'],
+                outputs='intermediate_data',
+                name='clean_data_node',
             )
         ]
     )
